b0631435710a39a9bb87b2c733c71ea56da4128d
Slipak Bohdan <fear3494@gmail.com>
2020-12-11 16:51:21 +0200

    test

---
+++ static/js/common.js
@@ -0,0 +1,13 @@
+// setTimeout(function(){
+//     $('.container-line').addClass('step-1').removeClass('step-0 ');
+// }, 2000);
+// setTimeout(function(){
+//     $('.container-line').addClass('step-2').removeClass('step-1');
+// }, 29000);
+// setTimeout(function(){
+//     $('.svg-animation').addClass('step-3');
+// }, 3000);
+// setTimeout(function(){
+//     $('.svg-animation').addClass('step-4');
+// }, 4000);
+//
